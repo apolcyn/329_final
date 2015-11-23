2e5c747e21e966bfc54691dfef4ccb485008428d
Alex <alexpolcyn65@gmail.com>
2015-11-23 13:35:55 -0800

    something

--- main.c
+++ main.c
@@ -1,155 +1,161 @@
-#include <msp430.h> 
+#include <msp430.h>
 
 #define VOLUME_UP 0x20DF40BF
 #define VOLUME_DOWN 0x20DFC03F
 #define CHANNEL_UP 0x20df00ff
 #define CHANNEL_DOWN 0x20DF807F
 
+#define ms_20        655   // 20.0 ms
+#define ms_0point75  25    // 0.75 ms
+#define ms_1point50  49    // 1.50 ms
+#define ms_2point25  73    // 2.25 ms
 
+int duty_cycles[] = {ms_0point75, ms_1point50, ms_2point25};
 
 int done = 1;
 long buf = 0;
 int index = 0;
 long timeCounter = 0;
 
+#define SERVO_PERIOD_COUNTS 400
+
+/*#define SERVO_POINT80 16
+#define SERVO_POINT50 30
+#define SERVO_POINT25 44*/
+
+#define SERVO_MINUS_30 24
+#define SERVO_NEUTRAL 30
+#define SERVO_PLUS_30 36
+
+int servo_count = 0;
+int low_servo = SERVO_NEUTRAL;
+int servo_on = 0;
+
+void set_servo_length(int duty_cycle) {
+    servo_count = 0;
+    low_servo = duty_cycle;
+}
+
 /*
  * main.c
  */
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
-    if (CALBC1_16MHZ==0xFF)	     // If calibration constant erased
+    WDTCTL = WDTPW | WDTHOLD;          // Stop watchdog timer
+
+    if (CALBC1_16MHZ==0xFF)              // If calibration constant erased
     {
-        while(1);                // do not load, trap CPU!!
+        while(1);                         // do not load, trap CPU!!
     }
-    DCOCTL = 0;                  // Select lowest DCOx and MODx settings
-    BCSCTL1 = CALBC1_1MHZ;      // Set range
+    DCOCTL = 0;                           // Select lowest DCOx and MODx settings
+    BCSCTL1 = CALBC1_1MHZ;                // Set range
     DCOCTL = CALDCO_1MHZ;
 
     CCTL0 |= CCIE;
     TACTL = TASSEL_2 + MC_1;
     TACCR0 = 100;
 
-    P1DIR |= BIT6;
+    P1DIR |= BIT6;                        // UsingP1.6 for LED.
 
-   // P1REN |= BIT0 + BIT1; // enable resistors on P1.1 and P1.0
-   // P1OUT |= BIT1;  // use a pull-up resistor on P1.1
-   // P1OUT &= ~BIT0;  // use a pull-down resistor on P1.0
-
-    P1IES |= BIT1;   //  This sets P1.1 to trigger on a falling edge.
-    P1IES &= ~BIT0;  //  This sets P1.0 to trigger on a rising edge
+    P1IES |= BIT1;                        // Set P1.1 to trigger on a falling edge.
+    P1IES &= ~BIT0;                       // Set P1.0 to trigger on a rising edge
     P1IE |= BIT0 + BIT1;
 
-    P1DIR |= BIT4;
+    P1DIR |= BIT5;
 
     __delay_cycles(250000);
-    P1IFG = 0;
+    P1IFG = 0;                            // Clear out unintended GPIO triggerings.
 
     __enable_interrupt();
 
-
-   // 00100000 11011111 00000000 11111111
-    // 0x20df00ff == channel up
-    // 0x20DF807F == channel down
-    // 0x20DF40BF == volume up
-    // 0x20DFC03F == volume down
+    servo_on = 1;
 
     while(1) {
-    	int temp = P1IFG;
-    	temp = temp;
-    	P1IFG = 0;
-        while(index < 32)
-        	;
-        index = 0;
-        done = 1;
+    buf = 0;                          // Reset buffer.
+        while(index < 32)                 // Sit here until we decode a 32-bit instruction
+        ;                             // from TV remote.
+        index = 0;                        // Reset index, and set flag to indicate
+        done = 1;                         // that the next faling edge is not a data bit.
 
-        switch(buf) {
-        case CHANNEL_UP:
-        	P1OUT |= BIT6;
-        	break;
+        switch(buf) {                     // Perform some action based on the code
+        case CHANNEL_UP:                  // sent by the TV remote.
+            set_servo_length(SERVO_MINUS_30);
+        //shift_servo(duty_cycles[0]);
+        break;
 
         case CHANNEL_DOWN:
-        	P1OUT &= ~BIT6;
-        	break;
+            set_servo_length(SERVO_NEUTRAL);
+        //shift_servo(duty_cycles[1]);
+        break;
 
         case VOLUME_UP:
-        	P1OUT |= BIT6;
-        	__delay_cycles(500000);
-        	P1OUT &= ~BIT6;
-        	__delay_cycles(500000);
-        	P1OUT |= BIT6;
-        	__delay_cycles(500000);
-        	P1OUT &= ~BIT6;
-        	break;
+            set_servo_length(SERVO_PLUS_30);
+        //shift_servo(duty_cycles[2]);
+        break;
 
         case VOLUME_DOWN:
-        	P1OUT ^= BIT6;
-        	break;
-        default:
-        	//error
-        	index = 0;
-        	break;
+        P1OUT ^= BIT6;
+        break;
+        default:                           // Error condition. Read an invalid code.
+        index = 0;
+        break;
         }
     }
 
-    //TACCR0 = 1;
-    //CCTL0 = CCIE; 24 bits in 14ms.
-	return 0;
+return 0;
 }
 
+/* Increments timeCounter, which counts up with a 100us period. Used to decode IR. */
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void something(void) {
-	timeCounter++;
+timeCounter++;
+if(servo_on) {
+   if(servo_count++ == low_servo) {
+       P1OUT &= ~BIT5;
+   }
+   else if(servo_count++ >= SERVO_PERIOD_COUNTS) {
+       servo_count = 0;
+       P1OUT |= BIT5;
+   }
+}
 }
 
+/* Reads the next data bit from IR sensor and stores it in a buffer.
+ * Using NEC protocol, so decodes '1's or '0's based on width of high signal.
+ */
 #pragma vector=PORT1_VECTOR
 __interrupt void button(void) {
-	P1OUT |= BIT4;
-	if(P1IFG & BIT0 && P1IFG & BIT1) {
-		// error condition. should not be both high at same time.
-		P1OUT ^= BIT6;
-	}
-	else if(P1IFG & BIT0) { //rising edge
-		if(done) {
-			done = 0;
-		}
-		timeCounter = 0;
-	}
-	else if(P1IFG & BIT1 && !done) { // falling edge
-		long diff = timeCounter;
-
-		if(diff < 8) {
-			// '0'
-			index++;
-			buf <<= 1;
-		}
-		else if(diff < 20) {
-			// '1'
-			index++;
-			buf <<= 1;
-			buf |= 1;
-		}
-		else if(diff < 60) {
-			// header
-			if(buf | index != 0) {
-				P1OUT |= BIT6;
-			}
-		}
-		else {
-			buf = 0;
-			index = 0;
-		}
-	}
-	__delay_cycles(100); // Delay a short bit just to prevent possible unintended edge triggerings
-	P1OUT &= ~BIT4;
-	P1IFG = 0;
+if(P1IFG & BIT0 && P1IFG & BIT1) {    // Check error condition.
+P1OUT ^= BIT6;                    // Shouldn't have interrupts for both high to low
+__delay_cycles(250000);           // and low to high transitions.
+P1OUT ^= BIT6;                    // Toggle LED to indicate error.
+__delay_cycles(250000);
+P1OUT ^= BIT6;
 }
-
-
-
-//1.714 bits per ms
-//1714.3 bits per sec
-//3428 bits per sec
-
-// 1888 bits per sec
-// 3600 Hz
+else if(P1IFG & BIT0) {               // Rising edge from IR receiver digital output.
+if(done) {                        // Indicate that subsequent edges are of interest.
+done = 0;
+}
+timeCounter = 0;                  // Reset time counter to zero on rising edge.
+}
+else if(P1IFG & BIT1 && !done) {      // Falling edge from IR receiver digital output.
+long diff = timeCounter;          // See how many 100us periods have gone by since
+                                          // the previous rising edge.
+if(diff < 8) {                    // This high signal was less than 800us long,                          // '0'
+index++;                      // which inidicates a '0' with NEC protocol.
+buf <<= 1;                    // Add in a '0' bit to the buffer.
+}
+else if(diff < 20) {              // This high signal is between 800us and 2ms,
+index++;                      // which inidicates a '1' bt by NEC protocol.
+buf <<= 1;                    // Add in a '1' bit to the buffer.
+buf |= 1;
+}
+else if(diff < 60) {              // This high signal is between 2ms and 6ms,
+if(buf | index != 0) {        // which inidicates that this was the data header.
+P1OUT |= BIT6;            // If buffer and index and not both set to zero,
+}                             // then this is an error condition, as these should
+}                                 // be reset before data capture.
+}
+__delay_cycles(100);                  // Delay a short bit just to prevent
+                                      // possible unintended edge triggerings.
+P1IFG = 0;                            // Clear Port 1 GPIO interrup flags
+}
